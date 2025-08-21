fb1b37ee1bc91262758c9b1e991314c51a57c8e2
Đặng Thông Nghĩa <dangnghia2385@gmail.com>
2025-08-22 01:34:55 +0700

    updaate

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
-      port: 5173, 
+    port: 5173, 
     host: true 
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '4173'),
+    allowedHosts: [
+      'portfolio-c027.onrender.com', 
+      'localhost',
+      '127.0.0.1'
+    ]
   },
   css: {
     devSourcemap: true
